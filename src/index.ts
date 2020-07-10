4f90ff49209bf9bb46ef3f0b6cab8f3cd97157fe
Pytness <pytness@gmail.com>
2020-07-10 07:37:50 +0200

    Update

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Core } from 'fc-premium-core'
+import $ from 'jquery'
+import mousetrap from 'mousetrap'
+
+export const lib = new Core.Module({
+	"name": "jquery-lib",
+	"description": "Autocomplete icons",
+	"author": "pytness",
+	"version": "1.0.1",
+	isLibrary: true,
+
+	"matches": ["*"]
+});
+
+lib.onload = function() {
+	console.log(Core)
+	Core.libraries.declare('jquery', $);
+	Core.libraries.declare('mousetrap', mousetrap);
+}
+
+export { lib as module };
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	watch: true,
+	entry: './out/index.js',
+	target: 'web',
+	output: {
+		path: __dirname,
+		filename: './index.js',
+		libraryTarget: 'assign',
+		library: 'module.exports',
+		// libraryExport: '',
+		// module: true
+		// library: '__module__'
+	},
+
+	mode: 'production',
+
+	optimization: {
+		minimize: false,
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					compress: {
+						// module: true,
+						ecma: 2017,
+						negate_iife: false,
+						unsafe: true,
+						unsafe_arrows: true,
+						arrows: true,
+
+						//
+						// top_retain: true,
+
+						// temp
+						// sequences: false
+					},
+
+					output: {
+						ecma: 2017,
+						// beautify: true,
+						// wrap_iife: true,
+						// indent_level: 8
+					}
+				}
+			})
+		]
+	},
+
+	externals: [{
+			'fc-premium-core': 'fcpremium',
+		},
+
+		function (context, request, callback) {
+			const exp = /^@fc-lib\/(.*)$/.exec(request);
+
+			if (exp !== null && exp[1].length !== 0)
+				return callback(null, `fcpremium.Core.libraries.import('${exp[1]}')`);
+
+			callback();
+		}
+	],
+
+	module: {
+		rules: [{
+			test: /\.(txt|css)$/,
+			use: [{
+				loader: 'raw-loader',
+			}]
+		}]
+	},
+
+	resolve: {
+		alias: {
+			'@assets': path.resolve(__dirname, 'assets/')
+		}
+	}
+
+};
